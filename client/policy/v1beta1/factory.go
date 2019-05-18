92deef2cc6d5aa7657840b05f305265f94408c14
Tamal Saha <tamal@appscode.com>
2019-05-18 08:48:45 -0700

    Implement SecurityPolicy
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ client/policy/v1beta1/factory.go
@@ -0,0 +1,312 @@
+package v1beta1
+
+import (
+	"fmt"
+	"github.com/appscode/go/types"
+	policy "k8s.io/api/policy/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/integer"
+	scc "kmodules.xyz/openshift/apis/security/v1"
+	api "kmodules.xyz/security-policy-api/apis/policy/v1beta1"
+	"math"
+)
+
+func ConvertToSecurityPolicy(obj runtime.Object) (*api.SecurityPolicy, error) {
+	switch t := obj.(type) {
+	case *policy.PodSecurityPolicy:
+		return FromPodSecurityPolicy(t), nil
+	case *scc.SecurityContextConstraints:
+		return FromSecurityContextConstraints(t), nil
+	default:
+		return nil, fmt.Errorf("the object is not a security policy")
+	}
+}
+
+func ToPodSecurityPolicy(in *api.SecurityPolicy) *policy.PodSecurityPolicy {
+	if in == nil {
+		return nil
+	}
+	return &policy.PodSecurityPolicy{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: policy.SchemeGroupVersion.String(),
+			Kind:       api.KindPodSecurityPolicy,
+		},
+		ObjectMeta: in.ObjectMeta,
+		Spec:       in.Spec.PodSecurityPolicySpec,
+	}
+}
+
+func FromPodSecurityPolicy(in *policy.PodSecurityPolicy) *api.SecurityPolicy {
+	if in == nil {
+		return nil
+	}
+	return &api.SecurityPolicy{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: policy.SchemeGroupVersion.String(),
+			Kind:       api.KindPodSecurityPolicy,
+		},
+		ObjectMeta: in.ObjectMeta,
+		Spec: api.SecurityPolicySpec{
+			PodSecurityPolicySpec: in.Spec,
+		},
+	}
+}
+
+func ToSecurityContextConstraints(in *api.SecurityPolicy) *scc.SecurityContextConstraints {
+	if in == nil {
+		return nil
+	}
+	return &scc.SecurityContextConstraints{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: scc.GroupVersion.String(),
+			Kind:       api.KindSecurityContextConstraints,
+		},
+		ObjectMeta: in.ObjectMeta,
+
+		AllowedCapabilities:             in.Spec.AllowedCapabilities,
+		AllowedFlexVolumes:              toAllowedFlexVolumes(in.Spec.AllowedFlexVolumes),
+		AllowedUnsafeSysctls:            in.Spec.AllowedUnsafeSysctls,
+		AllowHostDirVolumePlugin:        len(in.Spec.AllowedHostPaths) > 0,
+		AllowHostIPC:                    in.Spec.HostIPC,
+		AllowHostNetwork:                in.Spec.HostNetwork,
+		AllowHostPID:                    in.Spec.HostPID,
+		AllowHostPorts:                  len(in.Spec.HostPorts) > 0,
+		AllowPrivilegeEscalation:        in.Spec.AllowPrivilegeEscalation,
+		DefaultAddCapabilities:          in.Spec.DefaultAddCapabilities,
+		DefaultAllowPrivilegeEscalation: in.Spec.DefaultAllowPrivilegeEscalation,
+		ForbiddenSysctls:                in.Spec.ForbiddenSysctls,
+		FSGroup:                         toFSGroupStrategyOptions(in.Spec.FSGroup),
+		Groups:                          in.Spec.Groups,
+		Priority:                        in.Spec.Priority,
+		ReadOnlyRootFilesystem:          in.Spec.ReadOnlyRootFilesystem,
+		RequiredDropCapabilities:        in.Spec.RequiredDropCapabilities,
+		RunAsUser:                       toRunAsUserStrategyOptions(in.Spec.RunAsUser),
+		SeccompProfiles:                 in.Spec.SeccompProfiles,
+		SELinuxContext:                  toSELinuxContextStrategyOptions(in.Spec.SELinux),
+		SupplementalGroups:              toSupplementalGroupsStrategyOptions(in.Spec.SupplementalGroups),
+		Users:                           in.Spec.Users,
+		Volumes:                         toFSTypes(in.Spec.Volumes),
+	}
+}
+
+func FromSecurityContextConstraints(in *scc.SecurityContextConstraints) *api.SecurityPolicy {
+	if in == nil {
+		return nil
+	}
+	return &api.SecurityPolicy{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: scc.GroupVersion.String(),
+			Kind:       api.KindSecurityContextConstraints,
+		},
+		ObjectMeta: in.ObjectMeta,
+		Spec: api.SecurityPolicySpec{
+			PodSecurityPolicySpec: policy.PodSecurityPolicySpec{
+				AllowedCapabilities:             in.AllowedCapabilities,
+				AllowedFlexVolumes:              fromAllowedFlexVolumes(in.AllowedFlexVolumes),
+				AllowedUnsafeSysctls:            in.AllowedUnsafeSysctls,
+				AllowedHostPaths:                fromAllowHostDirVolumePlugin(in.AllowHostDirVolumePlugin),
+				HostIPC:                         in.AllowHostIPC,
+				HostNetwork:                     in.AllowHostNetwork,
+				HostPID:                         in.AllowHostPID,
+				HostPorts:                       fromAllowHostPorts(in.AllowHostPorts),
+				AllowPrivilegeEscalation:        in.AllowPrivilegeEscalation,
+				DefaultAddCapabilities:          in.DefaultAddCapabilities,
+				DefaultAllowPrivilegeEscalation: in.DefaultAllowPrivilegeEscalation,
+				ForbiddenSysctls:                in.ForbiddenSysctls,
+				FSGroup:                         fromFSGroupStrategyOptions(in.FSGroup),
+				ReadOnlyRootFilesystem:          in.ReadOnlyRootFilesystem,
+				RequiredDropCapabilities:        in.RequiredDropCapabilities,
+				RunAsUser:                       fromRunAsUserStrategyOptions(in.RunAsUser),
+				SELinux:                         fromSELinuxContextStrategyOptions(in.SELinuxContext),
+				SupplementalGroups:              fromSupplementalGroupsStrategyOptions(in.SupplementalGroups),
+				Volumes:                         fromFSTypes(in.Volumes),
+			},
+			Groups:          in.Groups,
+			Priority:        in.Priority,
+			SeccompProfiles: in.SeccompProfiles,
+			Users:           in.Users,
+		},
+	}
+}
+
+func fromAllowHostDirVolumePlugin(allowHostDirVolumePlugin bool) []policy.AllowedHostPath {
+	if allowHostDirVolumePlugin {
+		return []policy.AllowedHostPath{}
+	}
+	return []policy.AllowedHostPath{
+		// making a safe bet that it is ok to read /tmp files
+		{
+			PathPrefix: "/tmp",
+			ReadOnly:   true,
+		},
+	}
+}
+
+func fromAllowHostPorts(allowHostPorts bool) []policy.HostPortRange {
+	if !allowHostPorts {
+		return nil
+	}
+	return []policy.HostPortRange{
+		{
+			Min: 0,
+			Max: 65535,
+		},
+	}
+}
+
+func toAllowedFlexVolumes(in []policy.AllowedFlexVolume) []scc.AllowedFlexVolume {
+	out := make([]scc.AllowedFlexVolume, 0, len(in))
+	for _, x := range in {
+		out = append(out, scc.AllowedFlexVolume{
+			Driver: x.Driver,
+		})
+	}
+	return out
+}
+
+func fromAllowedFlexVolumes(in []scc.AllowedFlexVolume) []policy.AllowedFlexVolume {
+	out := make([]policy.AllowedFlexVolume, 0, len(in))
+	for _, x := range in {
+		out = append(out, policy.AllowedFlexVolume{
+			Driver: x.Driver,
+		})
+	}
+	return out
+}
+
+func toFSGroupStrategyOptions(in policy.FSGroupStrategyOptions) scc.FSGroupStrategyOptions {
+	return scc.FSGroupStrategyOptions{
+		Type:   scc.FSGroupStrategyType(string(in.Rule)),
+		Ranges: toIDRange(in.Ranges),
+	}
+}
+
+func fromFSGroupStrategyOptions(in scc.FSGroupStrategyOptions) policy.FSGroupStrategyOptions {
+	return policy.FSGroupStrategyOptions{
+		Rule:   policy.FSGroupStrategyType(string(in.Type)),
+		Ranges: fromIDRange(in.Ranges),
+	}
+}
+
+func toIDRange(in []policy.IDRange) []scc.IDRange {
+	out := make([]scc.IDRange, 0, len(in))
+	for _, x := range in {
+		out = append(out, scc.IDRange{
+			Min: x.Min,
+			Max: x.Max,
+		})
+	}
+	return out
+}
+
+func fromIDRange(in []scc.IDRange) []policy.IDRange {
+	out := make([]policy.IDRange, 0, len(in))
+	for _, x := range in {
+		out = append(out, policy.IDRange{
+			Min: x.Min,
+			Max: x.Max,
+		})
+	}
+	return out
+}
+
+func toRunAsUserStrategyOptions(in policy.RunAsUserStrategyOptions) scc.RunAsUserStrategyOptions {
+	var out scc.RunAsUserStrategyOptions
+
+	out.Type = scc.RunAsUserStrategyType(string(in.Rule))
+	if len(in.Ranges) == 1 {
+		if in.Ranges[0].Min == in.Ranges[0].Max {
+			out.UID = types.Int64P(in.Ranges[0].Min)
+		} else {
+			out.Type = scc.RunAsUserStrategyMustRunAsRange
+			out.UIDRangeMin = types.Int64P(in.Ranges[0].Min)
+			out.UIDRangeMax = types.Int64P(in.Ranges[0].Max)
+		}
+	} else if len(in.Ranges) > 1 {
+		var min int64 = math.MaxInt64
+		var max int64 = math.MinInt64
+		for _, x := range in.Ranges {
+			min = integer.Int64Min(min, x.Min)
+			max = integer.Int64Max(max, x.Max)
+		}
+
+		if min == max {
+			out.UID = types.Int64P(min)
+		} else {
+			out.Type = scc.RunAsUserStrategyMustRunAsRange
+			out.UIDRangeMin = types.Int64P(min)
+			out.UIDRangeMax = types.Int64P(max)
+		}
+	}
+	return out
+}
+
+func fromRunAsUserStrategyOptions(in scc.RunAsUserStrategyOptions) policy.RunAsUserStrategyOptions {
+	var out policy.RunAsUserStrategyOptions
+
+	out.Rule = policy.RunAsUserStrategy(string(in.Type))
+	if in.UID != nil {
+		out.Ranges = []policy.IDRange{
+			{
+				Min: *in.UID,
+				Max: *in.UID,
+			},
+		}
+	} else if in.UIDRangeMin != nil && in.UIDRangeMax != nil {
+		out.Ranges = []policy.IDRange{
+			{
+				Min: *in.UIDRangeMin,
+				Max: *in.UIDRangeMax,
+			},
+		}
+	}
+	return out
+}
+
+func toSELinuxContextStrategyOptions(in policy.SELinuxStrategyOptions) scc.SELinuxContextStrategyOptions {
+	return scc.SELinuxContextStrategyOptions{
+		Type:           scc.SELinuxContextStrategyType(string(in.Rule)),
+		SELinuxOptions: in.SELinuxOptions,
+	}
+}
+
+func fromSELinuxContextStrategyOptions(in scc.SELinuxContextStrategyOptions) policy.SELinuxStrategyOptions {
+	return policy.SELinuxStrategyOptions{
+		Rule:           policy.SELinuxStrategy(string(in.Type)),
+		SELinuxOptions: in.SELinuxOptions,
+	}
+}
+
+func toSupplementalGroupsStrategyOptions(in policy.SupplementalGroupsStrategyOptions) scc.SupplementalGroupsStrategyOptions {
+	if in.Rule == policy.SupplementalGroupsStrategyMayRunAs {
+		// error
+	}
+	return scc.SupplementalGroupsStrategyOptions{
+		Type:   scc.SupplementalGroupsStrategyType(string(in.Rule)),
+		Ranges: toIDRange(in.Ranges),
+	}
+}
+
+func fromSupplementalGroupsStrategyOptions(in scc.SupplementalGroupsStrategyOptions) policy.SupplementalGroupsStrategyOptions {
+	return policy.SupplementalGroupsStrategyOptions{
+		Rule:   policy.SupplementalGroupsStrategyType(string(in.Type)),
+		Ranges: fromIDRange(in.Ranges),
+	}
+}
+
+func toFSTypes(in []policy.FSType) []scc.FSType {
+	out := make([]scc.FSType, 0, len(in))
+	for _, x := range in {
+		out = append(out, scc.FSType(string(x)))
+	}
+	return out
+}
+
+func fromFSTypes(in []scc.FSType) []policy.FSType {
+	out := make([]policy.FSType, 0, len(in))
+	for _, x := range in {
+		out = append(out, policy.FSType(string(x)))
+	}
+	return out
+}
---
+++ go.mod
@@ -0,0 +1,43 @@
+module kmodules.xyz/security-policy-api
+
+go 1.12
+
+require (
+	cloud.google.com/go v0.39.0 // indirect
+	contrib.go.opencensus.io/exporter/ocagent v0.5.0 // indirect
+	github.com/Azure/go-autorest v12.0.0+incompatible // indirect
+	github.com/appscode/go v0.0.0-20190424183524-60025f1135c9
+	github.com/evanphx/json-patch v4.2.0+incompatible
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/gophercloud/gophercloud v0.0.0-20190516165734-b3a23cc94cc5 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.9.0 // indirect
+	github.com/json-iterator/go v1.1.6
+	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f // indirect
+	golang.org/x/net v0.0.0-20190514140710-3ec191127204 // indirect
+	golang.org/x/oauth2 v0.0.0-20190517181255-950ef44c6e07 // indirect
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
+	golang.org/x/sys v0.0.0-20190516110030-61b9204099cb // indirect
+	google.golang.org/appengine v1.6.0 // indirect
+	google.golang.org/genproto v0.0.0-20190516172635-bb713bdc0e52 // indirect
+	k8s.io/api v0.0.0-20190515023547-db5a9d1c40eb
+	k8s.io/apimachinery v0.0.0-20190515023456-b74e4c97951f
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/utils v0.0.0-20190506122338-8fab8cb257d5
+	kmodules.xyz/client-go v0.0.0-20190515205239-a16030cc2e50
+	kmodules.xyz/openshift v0.0.0-20190508141315-99ec9fc946bf
+)
+
+replace (
+	github.com/graymeta/stow => github.com/appscode/stow v0.0.0-20190506085026-ca5baa008ea3
+	k8s.io/api => k8s.io/api v0.0.0-20190313235455-40a48860b5ab
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.0.0-20190315093550-53c4693659ed
+	k8s.io/apimachinery => github.com/kmodules/apimachinery v0.0.0-20190508045248-a52a97a7a2bf
+	k8s.io/apiserver => github.com/kmodules/apiserver v0.0.0-20190508082252-8397d761d4b5
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.0.0-20190314001948-2899ed30580f
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.0.0-20190314002645-c892ea32361a
+	k8s.io/klog => k8s.io/klog v0.3.0
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.0.0-20190314000639-da8327669ac5
+	k8s.io/kube-openapi => k8s.io/kube-openapi v0.0.0-20190228160746-b3a7cee44a30
+	k8s.io/metrics => k8s.io/metrics v0.0.0-20190314001731-1bd6a4002213
+	k8s.io/utils => k8s.io/utils v0.0.0-20190221042446-c2654d5206da
+)
