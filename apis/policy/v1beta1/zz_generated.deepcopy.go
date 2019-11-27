1b4c8b525901fb3a462a4d1b6e479d429896639b
Tamal Saha <tamal@appscode.com>
2019-11-27 06:56:36 -0800

    Update client-go to kubernetes-1.16.3
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/policy/v1beta1/zz_generated.deepcopy.go
+++ apis/policy/v1beta1/zz_generated.deepcopy.go
@@ -55,7 +55,7 @@ func (in *SecurityPolicy) DeepCopyObject() runtime.Object {
 func (in *SecurityPolicyList) DeepCopyInto(out *SecurityPolicyList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	out.ListMeta = in.ListMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]SecurityPolicy, len(*in))
